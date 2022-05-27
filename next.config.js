977b160c45708e2bf083dedf2fb0ab8f7a1aac61
yunatuzuki <yuzunacchan@gmail.com>
2022-05-27 23:04:57 +0900

    環境変数不具合修正

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-loadEnv(process.env.API_KEY);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
-
-function loadEnv(appEnv = "local") {
-  const env = {
-    ...require(`./env/.env.development.${appEnv}`),
-    NEXT_PUBLIC_APP_ENV: appEnv,
-  };
-
-  Object.entries(env).forEach(([key, value]) => {
-    process.env[key] = value;
-  });
-}
